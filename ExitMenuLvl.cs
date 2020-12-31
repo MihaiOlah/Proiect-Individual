06065771f2b3c88f3717b5b3fd26fedd7d70027f
MihaiOlah <73793600+MihaiOlah@users.noreply.github.com>
2020-12-31 16:19:06 +0200

    Add files via upload

---
+++ ExitMenuLvl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ExitMenuLvl : MonoBehaviour
+{
+    public GameObject panel;
+    private bool activePanel;
+
+    public void Back(bool option)
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //panel = GameObject.FindGameObjectWithTag("Panel");
+        //panel.SetActive(false);
+        //activePanel = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        //daca se apasa Esc, atunci daca panoul era dezactivat, este activat, altfel, este activat
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!activePanel)
+            {
+                panel.SetActive(true);
+                activePanel = true;
+                Time.timeScale = 0;     // se pune pauza jocului
+            }
+            else
+            {
+                panel.SetActive(false);
+                activePanel = false;
+                Time.timeScale = 1;     // se opreste pauza jocului
+            }
+        }
+        */
+    }
+}
---
+++ LevelSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelection : MonoBehaviour
+{
+    public Button[] buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //int levelAt = PlayerPrefs.GetInt("levelAt", 2);
+        int levelAt = PlayerPrefs.GetInt("levelAt");
+        for (int i = 0; i < buttons.Length; i++)
+            if (i + 2 > levelAt)
+                buttons[i].interactable = false;
+    }
+
+    public void LoadLevel(int levelIndex)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(levelIndex);
+    }
+}
